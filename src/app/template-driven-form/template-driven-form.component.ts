48f1f1f9d15c093f83ee91ed2a670ba70028e856
Korlan Omarova <omar_korlan@mail.ru>
2021-04-15 21:29:43 +0600

    commit

---
+++ src/app/template-driven-form/template-driven-form.component.ts
@@ -0,0 +1,13 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-template-driven-form',
+  templateUrl: './template-driven-form.component.html',
+  styleUrls: ['./template-driven-form.component.css']
+})
+export class TemplateDrivenFormComponent implements OnInit {
+
+  ngOnInit(): void {
+
+  }
+}
